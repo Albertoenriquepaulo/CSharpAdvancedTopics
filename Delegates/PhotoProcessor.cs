da1ff8a9f2538a28cd73e0b4b89914ebcaca175b
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-09-07 19:02:13 +0200

    7. Delegates

---
+++ Delegates/PhotoProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        // WITH generic delegates
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            var photo = Photo.Load(path);
+            filterHandler(photo);
+            photo.Save();
+        }
+
+        //// WITH custom delegates
+        //public delegate void PhotoFilterHandler(Photo photo);
+        //public void Process(string path, PhotoFilterHandler filterHandler)
+        //{
+        //    var photo = Photo.Load(path);
+        //    filterHandler(photo);
+        //    photo.Save();
+        //}
+
+        // WITHOUT delegates
+        //public void Process(string path)
+        //{
+        //    var photo = Photo.Load(path);
+        //    var filters = new PhotoFilters();
+        //    filters.ApplyBrightness(photo);
+        //    filters.ApplyContrast(photo);
+        //    filters.Resize(photo);
+
+        //    photo.Save();
+        //}
+
+        public void Save()
+        {
+
+        }
+
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is with PhotoProcessor without delegate
+            //PhotoProcessor photoProcessor = new PhotoProcessor().Process("Path");
+
+
+            //// This is with PhotoProcessor with custom delegate
+            //PhotoProcessor photoProcessor = new PhotoProcessor();
+            //PhotoProcessor.PhotoFilterHandler filterHandler = new PhotoFilters().ApplyBrightness;
+
+            // This is with PhotoProcessor with generic delegate
+            PhotoProcessor photoProcessor = new PhotoProcessor();
+            Action<Photo> filterHandler = new PhotoFilters().ApplyBrightness;
+
+            filterHandler += new PhotoFilters().ApplyContrast;
+            filterHandler += new PhotoFilters().Resize;
+            filterHandler += ((Photo photo) => { Console.WriteLine("Apply Remove Red Eye"); });
+            filterHandler += RemovedBordersFilter;
+
+            photoProcessor.Process("algo.jpg", filterHandler);
+
+        }
+
+        static void RemovedBordersFilter(Photo photo)
+        {
+            Console.WriteLine("Apply Removed Border");
+        }
+    }
+}
